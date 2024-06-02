5b275392f84af94ecac4eecf36e821ed28e38954
ARYAN PURI <aryanpuri600@gmail.com>
2024-06-02 23:00:34 +0530

    new correct

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 export default function NavBar() {
   return (
       <nav className="bg-slate-900 flex place-content-center py-2">
-          <Link href="/" className="!bg-white mx-auto rounded-lg text-black py-1 px-3 font-medium">Home</Link>
-          <Link href="/addUser" className="!bg-white mx-auto rounded-lg text-black py-1 px-3 font-medium">Add User</Link>
-          <Link href="/display" className="!bg-white mx-auto rounded-lg text-black py-1 px-3 font-medium">Search Existing User</Link>
-          <Link href="/records" className="!bg-white mx-auto rounded-lg text-black py-1 px-3 font-medium">Records</Link>
+          <Link href="/" className="bg-blue-900 mx-auto rounded-lg text-white py-1 px-3 font-medium">Home</Link>
+          <Link href="/addUser" className="bg-blue-900 mx-auto rounded-lg text-white py-1 px-3 font-medium">Add User</Link>
+          <Link href="/display" className="bg-blue-900 mx-auto rounded-lg text-white py-1 px-3 font-medium">Search Existing User</Link>
+          <Link href="/records" className="bg-blue-900 mx-auto rounded-lg text-white py-1 px-3 font-medium">Records</Link>
       </nav>
   );
 }
