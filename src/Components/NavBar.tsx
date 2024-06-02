047f16abe82b18bd53d4276b1b51b5d668cfab29
ARYAN PURI <aryanpuri600@gmail.com>
2024-06-02 23:17:33 +0530

    commit

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 export default function NavBar() {
   return (
-      <nav className="bg-slate-900 flex place-content-center py-2">
-          <Link href="/" className="bg-blue-900 mx-auto rounded-lg text-white py-1 px-3 font-medium">Home</Link>
-          <Link href="/addUser" className="bg-blue-900 mx-auto rounded-lg text-white py-1 px-3 font-medium">Add User</Link>
-          <Link href="/display" className="bg-blue-900 mx-auto rounded-lg text-white py-1 px-3 font-medium">Search Existing User</Link>
-          <Link href="/records" className="bg-blue-900 mx-auto rounded-lg text-white py-1 px-3 font-medium">Records</Link>
+      <nav className="bg-slate-900 flex place-content-center py-2 fixed top-0 w-[100vw]">
+          <Link href="/" className="bg-blue-900 mx-auto rounded-lg text-xl text-white py-1 px-3 font-bold">Home</Link>
+          <Link href="/addUser" className="bg-blue-900 mx-auto rounded-lg text-xl text-white py-1 px-3 font-bold">Add User</Link>
+          <Link href="/display" className="bg-blue-900 mx-auto rounded-lg text-xl text-white py-1 px-3 font-bold">Search Existing User</Link>
+          <Link href="/records" className="bg-blue-900 mx-auto rounded-lg text-xl text-white py-1 px-3 font-bold">Records</Link>
       </nav>
   );
 }
